2fd6bcda6117a8f5d991341479b7aece1b9f6298
Shyam Phani <shyamphani777@gmail.com>
2024-09-14 14:23:12 +0530

    episode01

---
+++ app.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("h1", { id: "heading" }, "hello world");
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is h1 tag"),
+    React.createElement("h2", {}, "This is h2 tag"),
+  ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
